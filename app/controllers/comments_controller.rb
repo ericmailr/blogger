93ab286a4433d6e9e8f5d33462ddedf15fed8ac0
ericmailr <ericmailr@gmail.com>
2017-09-23 21:35:41 -0600

    fix comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,9 @@ class CommentsController < ApplicationController
 
  		redirect_to article_path(@comment.article)
 	end
-
+	
 	def comment_params
 		params.require(:comment).permit(:author_name, :body)
 	end
+	
 end
